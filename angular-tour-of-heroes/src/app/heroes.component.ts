d69eb59ef7ea53506851177c8a9c2e1259cadfea
Chris Buggy <cbuggy@tssg.org>
2017-06-20 12:29:34 +0100

    finsihed Routing section

--- angular-tour-of-heroes/src/app/heroes.component.ts
+++ angular-tour-of-heroes/src/app/heroes.component.ts
@@ -2,28 +2,36 @@ import { Component, OnInit } from '@angular/core';
 import { HeroDetailComponent } from './hero-detail.component';
 import { Hero } from './hero';
 import { HeroService } from './hero.service';
+import { Router } from '@angular/router';
 
 
 @Component({
   selector: 'my-heroes',
-  template: `
-  <h2>My Heroes</h2>
-  <ul class="heroes">
-    <li *ngFor="let hero of heroes"
-      [class.selected]="hero === selectedHero"
-      (click)="onSelect(hero)">
-      <span class="badge">{{hero.id}}</span> {{hero.name}}
-    </li>
-  </ul>
-  <div *ngIf="selectedHero">
-  <h2> {{selectedHero.name | uppercase}} is my hero </h2>
-  <button (click)="gotoDetail()">View Details</button>
-  </div>
-`
+  templateUrl: './heroes.component.html',
+  styleUrls: [ './heroes.component.css']
 })
 
 export class HeroesComponent implements OnInit {
-  ngOnInit(){
-  //..
-}
+  heroes: Hero[];
+  selectedHero : Hero;
+
+  constructor(
+    private router: Router,
+    private HeroService: HeroService){}
+
+    getHeroes(): void {
+      this.HeroService.getHeroes().then(heroes => this.heroes = heroes);
+    }
+
+    ngOnInit(): void{
+      this.getHeroes();
+    }
+
+    onSelect(hero: Hero): void {
+      this.selectedHero = hero;
+    }
+
+    gotoDetail(): void {
+      this.router.navigate(['/detail', this.selectedHero.id]);
+    }
 }
