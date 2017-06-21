cbb7234aa17020edc1734e79d74972e0e207508f
Chris Buggy <cbuggy@tssg.org>
2017-06-21 16:40:10 +0100

    on ngOnInit in hero-searchcompoent

--- angular-tour-of-heroes/src/app/hero.service.ts
+++ angular-tour-of-heroes/src/app/hero.service.ts
@@ -10,7 +10,10 @@ export class HeroService{
 
   constructor(private http: Http){ }
 
-  getHero(id: number): Promise<Hero>
+  getHero(id: number): Promise<Hero> {
+    const url =`${this.heroesUrl}/${id}`;
+    return this.http.get(url).toPromise().then(response => response.json().data as Hero).catch(this.handleError);
+  }
 
   getHeroes(): Promise<Hero[]> {
     return this.http.get(this.heroesUrl).toPromise().then(response => response.json().data as Hero[]).catch(this.handleError);
@@ -19,5 +22,22 @@ export class HeroService{
   private handleError(error: any): Promise<any> {
     console.error('an error occured', error); // for demo purposes only
     return Promise.reject(error.message || error );
+  }
+
+  private headers = new Headers({'Content-Type': 'application/json'});
+
+  update(hero: Hero): Promise<Hero> {
+    const url =`${this.heroesUrl}/${hero.id}`;
+    return this.http.put(url, JSON.stringify(hero), {headers: this.headers}).toPromise().then(() => hero).catch(this.handleError);
+  }
+
+  create(name: string): Promise<Hero> {
+    return this.http.post(this.heroesUrl, JSON.stringify({name: name}),
+    {headers: this.headers}).toPromise().then(res => res.json().data as Hero).catch(this.handleError);
+  }
+
+  delete(id: number): Promise<void> {
+    const url = `${this.heroesUrl}/${id}`;
+    return this.http.delete(url, {headers: this.headers}).toPromise().then(() => null).catch(this.handleError);
   }
 }
