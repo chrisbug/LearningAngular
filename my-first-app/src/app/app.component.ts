846d3c929e0fb0d03948cf1675f009fde319d1d8
chrisBug <christopher199348@gmail.com>
2017-07-13 23:35:50 +0100

    added in myfirstapp

--- my-first-app/src/app/app.component.ts
+++ my-first-app/src/app/app.component.ts
@@ -1,9 +1,18 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-root',
+  selector: 'app-root',//this is basic and best one to use
+  //selector alternatives can use ['app-root'] to select by atributes e.g <div app-root></div>
+  // can also select by class e.g .app-root
+  // but can't use id and sudo selectors e.g click
   templateUrl: './app.component.html',
+  //can use template: to write html in file
   styleUrls: ['./app.component.css']
+  //styles: [`
+  //  h3{
+  //     color: red;
+  //   }
+  //  `]
 })
 export class AppComponent {
   title = 'app works!';
--- observables/src/app/posts.component.ts
+++ observables/src/app/posts.component.ts
@@ -1,18 +1,43 @@
-import { Component }      from '@angular/core';
-import { Http }           from '@angular/http';
-import { PostService }    from './posts.service';
+import { Component, OnInit, DoCheck }      from '@angular/core';
+import { Http }                            from '@angular/http';
+import { PostService }                     from './posts.service';
+import { Post }                            from './post';
 
 
 @Component({
   selector: 'posts',
   template: `
     <h1>Posts</h1>
+    <button (click)="clickMe()">Click</button>
+    <div *ngIf="isLoading">
+      <i class="fa fa-spinner fa-spin fa-3x" aria-hidden="true"></i>
+    </div>
+    <div *ngFor="let post of posts">
+      <h3>{{post.title}}</h3>
+      <p>{{post.body}}</p>
+      <hr />
+    </div>
   `
 })
-export class PostsComponents{
+export class PostsComponents implements OnInit, DoCheck {
+  posts: Post[];
+  isLoading = true;
+
   constructor(private _postService: PostService){
+  }
+
+  ngOnInit(){
     this._postService.getPosts().subscribe(posts => {
-      console.log(posts);
+      this.isLoading = false;
+      this.posts = posts;
     });
+  }
+
+  ngDoCheck(){
+    console.log('DoCheck has run....');
+  }
+
+  clickMe(){
+    console.log('clicked');
   }
 }
