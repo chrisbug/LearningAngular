986309f4e482477943f34f9f25b0c9ee8deb48cc
Chris Buggy <cbuggy@tssg.org>
2017-07-17 17:40:55 +0100

    done first iteration fo app;

--- basics-assignment-3-start/src/app/app.module.ts
+++ basics-assignment-3-start/src/app/app.module.ts
@@ -4,10 +4,16 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { ShoppingListComponent } from './shopping-list/shopping-list.component';
+import { RecipeBookComponent } from './recipe-book/recipe-book.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    ShoppingListComponent,
+    RecipeBookComponent
   ],
   imports: [
     BrowserModule,
