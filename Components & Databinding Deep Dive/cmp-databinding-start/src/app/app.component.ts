845b0e44ea71a0aa035d763136ce1fe146588797
Chris Buggy <cbuggy@tssg.org>
2017-07-19 14:02:00 +0100

    on 65

--- Components & Databinding Deep Dive/cmp-databinding-start/src/app/app.component.ts
+++ Components & Databinding Deep Dive/cmp-databinding-start/src/app/app.component.ts
@@ -1,10 +1,30 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  serverElements = [{type: 'server', name: 'Testserver', content: 'Just a test!'}];
+export class AppComponent implements OnInit {
+  serverElements = [{type: 'server', name: 'Testserver', content: 'Just a test!'}]; //the colons here are for js syntax key value pairs
+
+  onServerAdded(serverData: {serverName: string, serverContent: string} ) {
+  this.serverElements.push({
+    type: 'server',
+    name: serverData.serverName,
+    content: serverData.serverContent
+  });
+  }
+
+  onBlueprintAdded(blueprintData: {serverName: string, serverContent: string}) {
+    this.serverElements.push({
+      type: 'blueprint',
+      name: blueprintData.serverName,
+      content: blueprintData.serverContent
+    });
+  }
+
+  constructor(){ }
+
+  ngOnInit() {}
 }
--- Components & Databinding Deep Dive/cmp-databinding-start/src/app/server-element/server-element.component.ts
+++ Components & Databinding Deep Dive/cmp-databinding-start/src/app/server-element/server-element.component.ts
@@ -1,16 +1,19 @@
-import { Component, OnInit, input } from '@angular/core';
+import { Component, OnInit, Input, ViewEncapsulation } from '@angular/core';
 
 @Component({
   selector: 'app-server-element',
   templateUrl: './server-element.component.html',
-  styleUrls: ['./server-element.component.css']
+  styleUrls: ['./server-element.component.css'],
+  encapsulation: ViewEncapsulation.Emulated //set the view caspulation rember none makes the css views global
 })
 export class ServerElementComponent implements OnInit {
-  @Input()
+  @Input('srvElement')
   element :{type: string, name: string, content: string};
+
   constructor() { }
 
   ngOnInit() {
+
   }
 
 }
